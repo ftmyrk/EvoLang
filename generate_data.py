78e6473666190737cf8a71662d1d87cdb65ab2e0
Faruk Tamyurek <108225481+ftmyrk@users.noreply.github.com>
2024-11-20 12:09:53 -0800

    Update generate_data.py

--- generate_data.py
+++ generate_data.py
@@ -1,6 +1,8 @@
+# generate_data.py
+
 from transformers import AutoTokenizer, AutoModelForCausalLM
-import text_preprocessing_utils as preprocess_utils
-import text_generation_utils as gen_utils
+from utils import text_preprocessing_utils as preprocess_utils
+from utils import text_generation_utils as gen_utils
 import torch
 
 
@@ -10,14 +12,14 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype=torch.float16, device_map="auto")
 tokenizer.pad_token_id = tokenizer.eos_token_id
 # path = '/home/otamy001/EvoLang/2013_year.csv'
-old_event_csv = '/home/otamy001/EvoLang/2013_year.csv'
-new_event_csv = '/home/otamy001/EvoLang/2023_year.csv'
+old_event_csv = '/home/otamy001/EvoLang/Dataset/2013_year.csv'
+new_event_csv = '/home/otamy001/EvoLang/Dataset/2023_year.csv'
 old_events = preprocess_utils.load_and_preprocess_dataset(old_event_csv, text_column=0)
 new_events = preprocess_utils.load_and_preprocess_dataset(new_event_csv, text_column=0)
 
 old_results = gen_utils.process_in_batches(old_events, model, tokenizer)
-gen_utils.save_results_to_csv(old_results, "generated_responses_2013.csv")
+gen_utils.save_results_to_csv(old_results, "/home/otamy001/EvoLang/generated_data/generated_responses_2013.csv")
 new_results = gen_utils.process_in_batches(new_events, model, tokenizer)
-gen_utils.save_results_to_csv(new_results, "generated_responses_2023.csv")
+gen_utils.save_results_to_csv(new_results, "/home/otamy001/EvoLang/generated_data/generated_responses_2023.csv")
 
 
