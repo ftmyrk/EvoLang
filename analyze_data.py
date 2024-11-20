bea731be266f783089b1b39438a3da414116a73d
Faruk Tamyurek <108225481+ftmyrk@users.noreply.github.com>
2024-11-20 12:11:11 -0800

    Update analyze_data.py

--- analyze_data.py
+++ analyze_data.py
@@ -1,4 +1,6 @@
-import text_analysis_utils as analysis_utils
+# analyze_data.py
+
+from utils import text_analysis_utils as analysis_utils
 import pandas as pd
 
 # Load preprocessed and generated data
@@ -10,13 +12,13 @@ old_events = [{"text": text} for text in old_data["Original_Text"]]
 new_events = [{"text": text} for text in new_data["Original_Text"]]
 
 # Perform analysis
-analysis_utils.word_frequency_analysis(old_events, "2013 Articles")
-analysis_utils.word_frequency_analysis(new_events, "2023 Articles")
-analysis_utils.generate_wordcloud(old_events, "2013 Articles")
-analysis_utils.generate_wordcloud(new_events, "2023 Articles")
+analysis_utils.word_frequency_analysis(old_events, "2013 Articles", "/home/otamy001/EvoLang/outputs/word_freq_2013.png")
+analysis_utils.word_frequency_analysis(new_events, "2023 Articles", "/home/otamy001/EvoLang/outputs/word_freq_2023.png")
+analysis_utils.generate_wordcloud(old_events, "2013 Articles", "/home/otamy001/EvoLang/outputs/wordcloud_2013.png")
+analysis_utils.generate_wordcloud(new_events, "2023 Articles", "/home/otamy001/EvoLang/outputs/wordcloud_2023.png")
 
 # Sentiment analysis
 sentiment_2013 = analysis_utils.analyze_sentiment(old_events)
 sentiment_2023 = analysis_utils.analyze_sentiment(new_events)
-analysis_utils.visualize_sentiment(sentiment_2013, "2013 Articles")
-analysis_utils.visualize_sentiment(sentiment_2023, "2023 Articles")
+analysis_utils.visualize_sentiment(sentiment_2013, "2013 Articles", "/home/otamy001/EvoLang/outputs/visualize_sentiment_2013.png")
+analysis_utils.visualize_sentiment(sentiment_2023, "2023 Articles", "/home/otamy001/EvoLang/outputs/visualize_sentiment_2023.png")
